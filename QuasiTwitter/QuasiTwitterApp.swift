0dba2d468d3aa6f5035dee9bcac896a793c7cad9
JustTeRoR <pavlenok2000@gmail.com>
2021-11-14 18:57:05 +0300

    Initial Commit

---
+++ QuasiTwitter/QuasiTwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuasiTwitterApp.swift
+//  QuasiTwitter
+//
+//  Created by Сергей Павленок on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct QuasiTwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
