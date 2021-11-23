2717f4db2ee9b0a96ac1bdf3b1b14ed3580241c4
JustTeRoR <pavlenok2000@gmail.com>
2021-11-23 23:34:50 +0300

    Connected firebase

--- QuasiTwitter/ContentView.swift
+++ QuasiTwitter/ContentView.swift
@@ -6,11 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onAppear() {
+                Analytics.logEvent(AnalyticsEventScreenView,
+                                   parameters: [AnalyticsParameterScreenName: "\(ContentView.self)",
+                                               AnalyticsParameterScreenClass: "\(ContentView.self)"])
+            }
     }
 }
 
--- QuasiTwitter/QuasiTwitterApp.swift
+++ QuasiTwitter/QuasiTwitterApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct QuasiTwitterApp: App {
+    
+    // @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().onAppear {
+                FirebaseApp.configure()
+            }
         }
     }
+    
+//    class AppDelegate: NSObject, UIApplicationDelegate {
+//        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//            FirebaseApp.configure()
+//            return true
+//        }
+//    }
 }
